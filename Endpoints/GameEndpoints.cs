26a0ea5a43b99d6fc977be11fad83583b522a92c
joywin2003 <21a20.joywin@sjec.ac.in>
2025-03-14 09:50:59 +0530

    maintained the contract by using DTO to return

--- Endpoints/GameEndpoints.cs
+++ Endpoints/GameEndpoints.cs
@@ -44,7 +44,15 @@ public static class GameEndpoints
             };
             dbContext.Games.Add(game);
             dbContext.SaveChanges();
-            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+
+            GameDto gameDto = new(
+                game.Id,
+                game.Name,
+                game.Genre!.Name,
+                game.Price,
+                game.ReleaseDate
+            );
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, gameDto);
         });
 
         group.MapPut("/{id}", (int id, UpdateGameDto updateGame) =>
