85229e26ba936385e933338432e4279abd80e56a
joywin2003 <21a20.joywin@sjec.ac.in>
2025-03-14 09:20:44 +0530

    fix: added the migrations required to run

---
+++ Data/Migrations/20250314034834_Dbchanges.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Dbchanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Genre_GenreId",
+                table: "Games");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Games",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Genre_GenreId",
+                table: "Games",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Genre_GenreId",
+                table: "Games");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Games",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Genre_GenreId",
+                table: "Games",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id");
+        }
+    }
+}
